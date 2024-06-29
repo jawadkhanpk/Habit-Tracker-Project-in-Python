f663601c4b732613ce94eda1e0dc811284bab3a7
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-06-29 20:17:25 +0500

    Creating a Pixela User

--- Habit-Tracker.py
+++ Habit-Tracker.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "YOUR_TOKEN_HERE",
+    "username": "YOUR_USERNAME_HERE",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
+
